6531d0a713e400be056987d1b310b75cab15452f
ratulislam46 <arfanratul46@gmail.com>
2025-08-05 11:02:45 +0600

    connect with mongodb

--- src/app/components/ServiceSection.jsx
+++ src/app/components/ServiceSection.jsx
@@ -1,21 +1,35 @@
+import dbConnect from '@/lib/DbConect';
 import Image from 'next/image';
 import React from 'react';
 
 const ServiceData = async () => {
-    const res = await fetch("http://localhost:3000/services.json")
-    const allData = await res.json();
+    // const res = await fetch("http://localhost:3000/services.json")
+    // const allData = await res.json();
     // console.log(data);
+
+    const serviceCollection = dbConnect('paracticeData')
+    const allData = await serviceCollection.find({}).toArray();
+
     return (
-        <div className='grid grid-cols-2 md:grid-cols-3 gap-6'>
-            {allData.map(data =>
-                <div key={data._id}>
-                    <Image src={data.img}
-                        width={200}
-                        height={200}
-                        alt={data.title}>
-                    </Image>
-                </div>
-            )}
+        <div>
+            {/* service section title and description  */}
+            <h4 className='text-[#FF3811] font-bold text-xl text-center'>Service</h4>
+            <h2 className='font-bold text-4xl text-center mt-4'>Our Service Area</h2>
+            <p className='text-gray-500 text-center mt-8 mb-12'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+
+            {/* sevices image  */}
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-6'>
+                {allData.map(data =>
+                    <div key={data._id}>
+                        <Image src={data.img}
+                            layout="responsive"
+                            width={600}   
+                            height={400}
+                            alt={data.title}>
+                        </Image>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
---
+++ src/lib/DbConect.js
@@ -0,0 +1,14 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export default function dbConnect(collectionName) {
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ibgq1ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    return client.db(process.env.DB_NAME).collection(collectionName)
+}
